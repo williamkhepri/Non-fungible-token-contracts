62b85e1a405d32c0e8041deba107cc60435013ab
unkwn0wn <4558401+williamkhepri@users.noreply.github.com>
2022-01-18 11:23:51 +0100

    Actualizar ERC721.sol
    Actualización ERC721.sol

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -1,1 +1,12 @@
+// SPDK-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+/**
+* @dev ERC-721 non-fungible token standard.
+* ver https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md.
+*/
+interface ERC721
+{
+
+
+}
