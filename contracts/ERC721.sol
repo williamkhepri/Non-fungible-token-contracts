0ef84267e3d8d0632eba1a7fc4631ddfb0b1a93f
unkwn0wn <4558401+williamkhepri@users.noreply.github.com>
2022-01-19 12:43:17 +0100

    Actualizar ERC721.sol
    Actualización.

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -9,8 +9,28 @@ pragma solidity ^0.8.0;
 interface ERC721
 {
 /*
-Se emite cuando la propiedad de cualquier NFT cambia por cualquier mecanismo. Éste evento se emite cuando los NFT son creados.
+* Se emite cuando la propiedad de cualquier NFT cambia por cualquier mecanismo. Éste evento se emite cuando los NFT son creados.
+* ('from' == 0) y destruidos ('to' == 0). Excepción: durante la creación del contrato, cualquiera puede crear y asignar una cantidad
+* de NFT sin emitir Transferencia. En el momento de cualquier transferencia, la dirección aprobada para ese NFT (si corresponde) se restablece a ninguno.
+*/
+event Transfer(
+address indexed _from, 
+address indexed _to,
+uint256 indexed _tokenId
+);
+
+/**
+* @dev Esto se emite cuando se cambia o reafirma la dirección aprobada para un NFT. 
+* La dirección cero indica que no existe una dirección aprobada. Cuando se emite un evento Transfer, 
+* también indica que la dirección aprobada para ese NFT (si corresponde) se reestablece a ninguna.
+*/
+event Approval(
+address indexed _owner, 
+address indexed _operator, 
+bool _approved
+);
+/**
+* @notice 
 
 */
-
 }
