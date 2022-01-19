b4517e363003eeb20db584af25748123fd6ed611
unkwn0wn <4558401+williamkhepri@users.noreply.github.com>
2022-01-19 17:05:35 +0100

    Actualización ERC721
    Actualización

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -30,7 +30,28 @@ address indexed _operator,
 bool _approved
 );
 /**
-* @notice 
-
+* @notice Lanza a menos que 'msg.sender' sea el propietario actual, un operador autorizado o la
+* dirección aprobada para este NFT. Lanza si '_from' no es propietario actual. Lanza si '_to' es
+* la dirección cero. Lanza si '_tokenId' no es un NFT válido. Cuando se completa la transferencia, esta
+* función comprueba si '_to' es un contrato inteligente (tamaño de código > 0). Si es así, llama a 
+* 'onERC721Recived' en '_to' y lanza si el valor devuelto no es
+* 'bytes4(keccak256("onERC721Recived(dirección, uint256, bytes)"))'.
+* @dev Transfiere la propiedad de un NFT de una dirección a otra dirección. Esta función puede ser
+* cambiada a pagadero.
+* @param _from El propietario actual del NFT.
+* @param _to El nuevo dueño.
+* @param _tokenId El NFT a transferir.
+* @param _data Datos adicionales sin formato especificado, enviados en llamada a '_to'.
 */
+function safeTransferFrom(
+address _from, 
+address _to, 
+uint256 _tokenId, 
+bytes calldata _data
+)
+external;
+
+/**
+* @notice Esto funciona de manera idéntica a la otra función con un parámetro de datos adicional, excepto que esto
+*
 }
