9e59a90d4ca9ce502bf68e500b0673d4db6cf077
unkwn0wn <4558401+williamkhepri@users.noreply.github.com>
2022-01-24 10:45:10 +0100

    Quitar link
    quitar link

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 
 /**
 * @dev ERC-721 non-fungible token standard.
-* ver https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md.
-* implementar desde https://github.com/nibbstack/erc721/blob/master/src/contracts/tokens/erc721.sol
+* ver https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md. 
 */
 interface ERC721
 {
