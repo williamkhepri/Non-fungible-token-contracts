347a0497df5e8d991619e3e82b7073ac2abb5e93
unkwn0wn <4558401+williamkhepri@users.noreply.github.com>
2022-02-10 18:18:32 +0100

    Crear NF-Token-metadata.sol
    Falta terminar de traducir

---
+++ contracts/NF-Token-metadata.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./nf-token.sol";
+import "./erc721-metadata.sol";
+
+/**
+ * @dev Implementación de metadatos opcionales para el estándard de token no fungible ERC-721.
+ */
+contract NFTokenMetadata is
+  NFToken,
+  ERC721Metadata
+{
+
+  /**
+   * @dev Un nombre descriptivo para la colección de NFT.
+   */
+  string internal nftName;
+
+  /**
+   * @dev Un nombre abreviado para NFTokens.
+   */
+  string internal nftSymbol;
+
+  /**
+   * @dev Mapeo de NFT ID a metadatos uri.
+   */
+  mapping (uint256 => string) internal idToUri;
+
+  /**
+   * @notice Al implementar este contrato, no olvide configurar nftName y nftSymbol.
+   * @dev Constructor del contrato.
+   */
+  constructor()
+  {
+    supportedInterfaces[0x5b5e139f] = true; // ERC721Metadatos
+  }
+
+  /**
+   * @dev Devuelve un nombre descriptivo para una colección de NFTokens.
+   * @return _name Representing name.
+   */
+  function name()
+    external
+    override
+    view
+    returns (string memory _name)
+  {
+    _name = nftName;
+  }
+
+  /**
+   * @dev Devuelve un nombre abreviado para NFToken.
+   * @return _symbol Simbolo que representa.
+   */
+  function symbol()
+    external
+    override
+    view
+    returns (string memory _symbol)
+  {
+    _symbol = nftSymbol;
+  }
+
+  /**
+   * @dev Un URI distinto (RFC 3986) para un NFT dado.
+   * @param _tokenId Id para el que queremos la uri.
+   * @return URI del _tokenId.
+   */
+  function tokenURI(
+    uint256 _tokenId
+  )
+    external
+    override
+    view
+    validNFToken(_tokenId)
+    returns (string memory)
+  {
+    return _tokenURI(_tokenId);
+  }
+
+  /**
+   * @notice Esta es una función interna que se puede anular si se desea implementar una forma diferente
+   * de generar un token URI.
+   * @param _tokenId Id para el que queremos URI.
+   * @return URI del _tokenId.
+   */
+  function _tokenURI(
+    uint256 _tokenId
+  )
+    internal
+    virtual
+    view
+    returns (string memory)
+  {
+    return idToUri[_tokenId];
+  }
+
+  /**
+   * @notice Esta es una función interna que debe llamarse desde una función externa implementada por el usuario.
+   * burn function. Its purpose is to show and properly initialize data structures when using this
+   * implementation. Also, note that this burn implementation allows the minter to re-mint a burned
+   * NFT.
+   * @dev Burns a NFT.
+   * @param _tokenId ID of the NFT to be burned.
+   */
+  function _burn(
+    uint256 _tokenId
+  )
+    internal
+    override
+    virtual
+  {
+    super._burn(_tokenId);
+
+    delete idToUri[_tokenId];
+  }
+
+  /**
+   * @notice This is an internal function which should be called from user-implemented external
+   * function. Its purpose is to show and properly initialize data structures when using this
+   * implementation.
+   * @dev Set a distinct URI (RFC 3986) for a given NFT ID.
+   * @param _tokenId Id for which we want URI.
+   * @param _uri String representing RFC 3986 URI.
+   */
+  function _setTokenUri(
+    uint256 _tokenId,
+    string memory _uri
+  )
+    internal
+    validNFToken(_tokenId)
+  {
+    idToUri[_tokenId] = _uri;
+  }
+
+}
